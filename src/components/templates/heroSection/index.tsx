db5d7579f273fad9a8cd3719951804598616fd94
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-06-10 11:03:01 +0100

    update

--- src/components/templates/heroSection/index.tsx
+++ src/components/templates/heroSection/index.tsx
@@ -50,14 +50,8 @@ export function HeroSection({ className, variant = "desktop" }: HeroSectionProps
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 0.3 }}
       >
-<Image 
-  src="/images/fredrick.jpeg" 
-  alt="Profile Picture" 
-  width={1000} 
-  height={1000} 
-  className="object-cover" 
-  priority 
-/>      </motion.div>
+        <img src="/images/fredrick.jpeg" alt="Profile" className="object-cover object-center w-full h-full" />
+      </motion.div>
     </div>
   )
 }
